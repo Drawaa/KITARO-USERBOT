1d28e443ff20c8378a1f0d4a29c83455e0eb15ef
Sayonara <89360980+krisnadiwangga@users.noreply.github.com>
2021-12-07 02:47:05 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -128,7 +128,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/Kitaroo/KITARO-USERBOT")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "Kitaro-Userbot")
+    "UPSTREAM_REPO_BRANCH", "KITARO-USERBOT")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
