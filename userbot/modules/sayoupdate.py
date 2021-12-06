634c47ca6d00e065c3ba088940991bc5cd78db36
Sayonara <89360980+krisnadiwangga@users.noreply.github.com>
2021-12-07 02:35:15 +0700

    Update sayoupdate.py

--- userbot/modules/sayoupdate.py
+++ userbot/modules/sayoupdate.py
@@ -55,7 +55,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
         if HEROKU_APP_NAME is None:
             await event.edit(
                 '`[HEROKU]: Harap Siapkan Variabel` **HEROKU_APP_NAME** `'
-                ' untuk dapat deploy perubahan terbaru dari Sayonara-Userbot.`'
+                ' untuk dapat deploy perubahan terbaru dari KITARO-USERBOT.`'
             )
             repo.__del__()
             return
